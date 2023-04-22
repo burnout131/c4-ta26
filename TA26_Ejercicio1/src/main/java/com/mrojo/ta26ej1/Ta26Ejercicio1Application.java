95d8a43df4f8acfd428e72a6ac8a16c9c5e3be5c
burnout131 <marcrg07@gmail.com>
2023-04-22 16:53:40 +0200

    Ejercicio1 initial commit

---
+++ TA26_Ejercicio1/src/main/java/com/mrojo/ta26ej1/Ta26Ejercicio1Application.java
@@ -0,0 +1,13 @@
+package com.mrojo.ta26ej1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ta26Ejercicio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ta26Ejercicio1Application.class, args);
+	}
+
+}
