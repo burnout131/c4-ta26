b786ead894b4e75ec2ecc2151d9c9e7b68d12705
burnout131 <marcrg07@gmail.com>
2023-04-22 23:28:50 +0200

    Ejercicio 2 initial commit

---
+++ TA26_Ejercicio2/src/main/java/com/mrojo/ta26ej2/Ta26Ejercicio2Application.java
@@ -0,0 +1,13 @@
+package com.mrojo.ta26ej2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ta26Ejercicio2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ta26Ejercicio2Application.class, args);
+	}
+
+}
---
+++ TA26_Ejercicio2/src/test/java/com/mrojo/ta26ej2/Ta26Ejercicio2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrojo.ta26ej2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ta26Ejercicio2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
