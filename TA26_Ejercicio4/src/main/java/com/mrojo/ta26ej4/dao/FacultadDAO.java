2e5a45afa1889c75b2a8bdd8806482c02f8eaea1
burnout131 <marcrg07@gmail.com>
2023-04-23 15:41:17 +0200

    Ejercicio 4 ok

---
+++ TA26_Ejercicio4/src/main/java/com/mrojo/ta26ej4/dao/FacultadDAO.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.mrojo.ta26ej4.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mrojo.ta26ej4.dto.Facultad;
+
+/**
+ * @author Marc
+ *
+ */
+public interface FacultadDAO extends JpaRepository<Facultad, Long> {
+
+}
---
+++ TA26_Ejercicio4/src/main/java/com/mrojo/ta26ej4/dao/ReservaDAO.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.mrojo.ta26ej4.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mrojo.ta26ej4.dto.Reserva;
+
+/**
+ * @author Marc
+ *
+ */
+public interface ReservaDAO extends JpaRepository<Reserva, Long> {
+
+}
